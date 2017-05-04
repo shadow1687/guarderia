e118f8b451136997103c18b55d2940a248b400f8
shadow1687 <shadow1687@gmail.com>
2017-05-04 00:08:49 -0300

    second commit

--- application/models/Alumno_model.php
+++ application/models/Alumno_model.php
@@ -22,11 +22,12 @@ class Alumno_model extends Generic_model {
 														 {$data['edad']},
 														 {$this -> db -> escape($data['direccion'])},
 														 {$this -> db -> escape($data['ciudad'])}
-													   );"
+													 );";
+
 		$qry[count($qry)]="SET @id=SELECT last_insert_id();";
 		//genero el alumno vacio
 		$qry[count($qry)]="INSERT INTO alumno (@id,0,0,0,sysdate());";
-		
+
 		return $this -> qry_exec($qry,$this -> db,"simple",array("manage_exception" => TRUE));
 	}
 
--- application/models/Generic_model.php
+++ application/models/Generic_model.php
@@ -9,33 +9,49 @@ class generic_model extends CI_Model {
 public function qry_exec($query,$db,$type,$config=array()){
   $manage_exception=(isset($config["manage_exception"]) ? TRUE:FALSE);
   $result=FALSE;
-	foreach ($query as $key => $value) {
-		switch($type){
-	    case 'simple' : {
-	                      $qry = $this->db->simple_query($query);
-	                      $result = $qry;
-	    }break;
-	    case 'array' :
-	    case 'row'   :
-	    case 'value' :{
-	                    $qry= $this->db->query($query);
-	                    switch($type){
-	                        case 'array'  : $result = $qry -> result_array();break;
-	                        case 'row'    : $result = $qry -> row();break;
-	                        case 'value'  : {
-	                                          $row = $qry -> row_array();
 
-																						if(is_array($row)){
-																							$result = reset($row);
-																						}
-	                                          else{
-																							$result=NULL;
-																						}
-	                        }break;
-	                    }
-	    }break;
-	  }
+	$execute=($type=='simple')? 'simple_query':'query';
+
+	if(is_array($query)){
+		$count=count($query);
+		$ind=1;
+		foreach ($query as $qry) {
+			if($ind!=$count){
+				$db -> simple_query($qry);
+			}
+			else{
+				$d -> $execute($qry);
+			}
+			$ind++;
+		}
 	}
+
+
+	switch($type){
+    case 'simple' : {
+                      $qry = $this->db->simple_query($query);
+                      $result = $qry;
+    }break;
+    case 'array' :
+    case 'row'   :
+    case 'value' :{
+                    $qry= $this->db->query($query);
+                    switch($type){
+                        case 'array'  : $result = $qry -> result_array();break;
+                        case 'row'    : $result = $qry -> row();break;
+                        case 'value'  : {
+                                          $row = $qry -> row_array();
+
+																					if(is_array($row)){
+																						$result = reset($row);
+																					}
+                                          else{
+																						$result=NULL;
+																					}
+                        }break;
+                    }
+    }break;
+  }
 
   if(!$result){
     if($manage_exception){
