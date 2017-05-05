90cf0aeb1f0d074435008a146f2936c3914cdf03
shadow1687 <shadow1687@gmail.com>
2017-05-04 21:59:43 -0300

    backup de la base de datos para pruebas

--- guarderia.sql
+++ guarderia.sql
@@ -1,40 +1,129 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.19-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.4.0.5144
--- --------------------------------------------------------
+-- MySQL dump 10.14  Distrib 5.5.52-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: guarderia
+-- ------------------------------------------------------
+-- Server version	5.5.52-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Volcando estructura para tabla guarderia.alumno
-CREATE TABLE IF NOT EXISTS `alumno` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) DEFAULT NULL,
-  `apellido` varchar(100) DEFAULT NULL,
-  `edad` int(11) DEFAULT NULL,
-  `nacimiento` date DEFAULT NULL,
-  `padre` bigint(20) DEFAULT NULL,
-  `dni` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `alumno`
+--
 
--- La exportación de datos fue deseleccionada.
--- Volcando estructura para tabla guarderia.aula
-CREATE TABLE IF NOT EXISTS `aula` (
+DROP TABLE IF EXISTS `alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumno` (
+  `id_persona` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_aula` varchar(100) NOT NULL DEFAULT '',
+  `turno` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `st` tinyint(1) NOT NULL DEFAULT '0',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_persona`,`id_aula`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno`
+--
+
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+INSERT INTO `alumno` VALUES (2,'1',1,0,'2017-05-01 22:35:11');
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aula`
+--
+
+DROP TABLE IF EXISTS `aula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aula` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `capacidad` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
--- Volcando estructura para tabla guarderia.guarderia
-CREATE TABLE IF NOT EXISTS `guarderia` (
+--
+-- Dumping data for table `aula`
+--
+
+LOCK TABLES `aula` WRITE;
+/*!40000 ALTER TABLE `aula` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aula_maestro`
+--
+
+DROP TABLE IF EXISTS `aula_maestro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aula_maestro` (
+  `id_aula` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_maestro` varchar(100) NOT NULL DEFAULT '',
+  `st` tinyint(1) NOT NULL DEFAULT '0',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_aula`,`id_maestro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aula_maestro`
+--
+
+LOCK TABLES `aula_maestro` WRITE;
+/*!40000 ALTER TABLE `aula_maestro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aula_maestro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estab_aula`
+--
+
+DROP TABLE IF EXISTS `estab_aula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estab_aula` (
+  `id_estab` bigint(20) NOT NULL,
+  `id_aula` bigint(20) NOT NULL,
+  `st` tinyint(1) NOT NULL DEFAULT '0',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_estab`,`id_aula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estab_aula`
+--
+
+LOCK TABLES `estab_aula` WRITE;
+/*!40000 ALTER TABLE `estab_aula` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estab_aula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `establecimiento`
+--
+
+DROP TABLE IF EXISTS `establecimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `establecimiento` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `responsable` bigint(20) DEFAULT NULL,
@@ -43,31 +132,108 @@ CREATE TABLE IF NOT EXISTS `guarderia` (
   `telefono` varchar(50) DEFAULT NULL,
   `ciudad` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
--- Volcando estructura para tabla guarderia.padre
-CREATE TABLE IF NOT EXISTS `padre` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Dumping data for table `establecimiento`
+--
+
+LOCK TABLES `establecimiento` WRITE;
+/*!40000 ALTER TABLE `establecimiento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `establecimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persona` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tipo` smallint(5) unsigned NOT NULL DEFAULT '0',
   `nombre` varchar(100) DEFAULT NULL,
   `apellido` varchar(100) DEFAULT NULL,
   `edad` int(11) DEFAULT NULL,
+  `nacimiento` date DEFAULT NULL,
   `dni` varchar(10) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `direccion` varchar(100) DEFAULT NULL,
   `ciudad` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
--- Volcando estructura para tabla guarderia.usuarios
-CREATE TABLE IF NOT EXISTS `usuarios` (
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` VALUES (1,0,'padre','ejemplo',28,'0000-00-00','1355113813','','',''),(2,2,'maria','alcantara',5,'2012-12-12','50000000','','',''),(3,2,'nombre1','apellido1',5,'0000-00-00','65165165','asdami@hotmail.com','a','a'),(4,0,'madre','ejemplo',25,'1991-12-01','42568978','','',''),(5,3,'profe','ape_profe',23,'1994-01-05','40005897','','','');
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relacion`
+--
+
+DROP TABLE IF EXISTS `relacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relacion` (
+  `id1` int(10) unsigned NOT NULL,
+  `id2` int(10) unsigned NOT NULL,
+  `st` tinyint(1) NOT NULL DEFAULT '0',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id1`,`id2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relacion`
+--
+
+LOCK TABLES `relacion` WRITE;
+/*!40000 ALTER TABLE `relacion` DISABLE KEYS */;
+INSERT INTO `relacion` VALUES (2,1,0,'2017-05-04 23:14:14'),(2,4,0,'2017-05-04 23:14:23'),(2,5,3,'2017-05-04 23:23:09');
+/*!40000 ALTER TABLE `relacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
   `username` varchar(20) NOT NULL,
   `password` varchar(500) DEFAULT NULL,
+  `persona` bigint(20) DEFAULT NULL,
   `type` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('admin','fe01ce2a7fbac8fafaed7c982a04e229',NULL,NULL),('padre','41409f34ee49036ad153fa10b374747e',1,NULL);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-04 21:57:06
